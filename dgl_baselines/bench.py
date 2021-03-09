977001c5996177d281be236ae544011b125e9c29
Yuke Wang <505008605@qq.com>
2021-03-09 10:53:56 -0800

    update with the files

--- dgl_baselines/bench.py
+++ dgl_baselines/bench.py
@@ -8,24 +8,24 @@ data_dir = '/home/yuke/.graphs/orig/'
 print(data_dir)
 
 dataset = [
-		('citeseer'	        , 3703	    , 6   ),  
-		('cora' 	        , 1433	    , 7   ),  
-		('pubmed'	        , 500	    , 3   ),      
-		('ppi'	        	, 50	    , 121 ),   
+		# ('citeseer'	        , 3703	    , 6   ),  
+		# ('cora' 	        , 1433	    , 7   ),  
+		# ('pubmed'	        , 500	    , 3   ),      
+		# ('ppi'	        	, 50	    , 121 ),   
 
 		# ('PROTEINS_full'             , 29       , 2) ,   
 		# ('OVCAR-8H'                  , 66       , 2) , 
 		# ('Yeast'                     , 74       , 2) ,
 		# ('DD'                        , 89       , 2) ,
 		# ('YeastH'                    , 75       , 2) ,   
+
+		( 'amazon0505'               , 96	  , 22),
+		( 'artist'                   , 100	  , 12),
+		( 'com-amazon'               , 96	  , 22),
+		( 'soc-BlogCatalog'	         , 128	  , 39),      
+		( 'amazon0601'  	         , 96	  , 22), 
+
 		# ('SW-620H'                   , 66       , 2) ,
-
-		# ( 'amazon0505'               , 96	  , 22),
-		# ( 'artist'                   , 100	  , 12),
-		# ( 'com-amazon'               , 96	  , 22),
-		# ( 'soc-BlogCatalog'	         , 128	  , 39),      
-		# ( 'amazon0601'  	         , 96	  , 22), 
-
 	    # ( 'reddit'                   , 602    , 41),
 		# ( 'web-BerkStan'             , 100	  , 12),
 		# ( 'wiki-topcats'             , 300	  , 12),
--- dgl_baselines/train.py
+++ dgl_baselines/train.py
@@ -272,7 +272,7 @@ if __name__ == '__main__':
     parser.add_argument("--lr", type=float, default=1e-2, help="learning rate")
     parser.add_argument("--n-epochs", type=int, default=10, help="number of training epochs")
     parser.add_argument("--n-hidden", type=int, default=16, help="number of hidden gcn units")
-    parser.add_argument("--n-layers", type=int, default=1, help="number of hidden gcn layers")
+    parser.add_argument("--n-layers", type=int, default=4, help="number of hidden gcn layers")
     parser.add_argument("--weight-decay", type=float, default=5e-4, help="Weight for L2 loss")
     parser.add_argument("--self-loop", action='store_true', help="graph self-loop (default=False)")
     parser.set_defaults(self_loop=True)
