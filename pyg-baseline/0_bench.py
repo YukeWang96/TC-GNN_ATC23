ac7009fba17f2047acc5b8f9c7fc5102ca5f5022
Yuke Wang <505008605@qq.com>
2021-04-02 15:33:41 -0700

    update pyg-baseline

--- pyg-baseline/0_bench.py
+++ pyg-baseline/0_bench.py
@@ -6,12 +6,12 @@ hidden = [16]
 # hidden = [32]
 
 dataset = [
-        # ('citeseer'	        , 3703	    , 6   ),  
-        # ('cora' 	        , 1433	    , 7   ),  
-        # ('pubmed'	        , 500	    , 3   ),      
-        # ('ppi'	            , 50	    , 121 ),   
+        ('citeseer'	        , 3703	    , 6   ),  
+        ('cora' 	        , 1433	    , 7   ),  
+        ('pubmed'	        , 500	    , 3   ),      
+        ('ppi'	            , 50	    , 121 ),   
 
-        # ('PROTEINS_full'             , 29       , 2) ,   
+        ('PROTEINS_full'             , 29       , 2) ,   
         ('OVCAR-8H'                  , 66       , 2) , 
         ('Yeast'                     , 74       , 2) ,
         ('DD'                        , 89       , 2) ,
@@ -19,11 +19,11 @@ dataset = [
         # ('TWITTER-Real-Graph-Partial', 1323     , 2) ,   
         # ('SW-620H'                   , 66       , 2) ,
 
-        # ( 'amazon0505'               , 96	  , 22),
-        # ( 'artist'                   , 100  , 12),
-        # ( 'com-amazon'               , 96	  , 22),
-        # ( 'soc-BlogCatalog'	         , 128  , 39), 
-        # ( 'amazon0601'  	         , 96	  , 22), 
+        ( 'amazon0505'               , 96	  , 22),
+        ( 'artist'                   , 100  , 12),
+        ( 'com-amazon'               , 96	  , 22),
+        ( 'soc-BlogCatalog'	         , 128  , 39), 
+        ( 'amazon0601'  	         , 96	  , 22), 
 ]
 
 
