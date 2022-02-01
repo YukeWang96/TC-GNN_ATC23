c5542af6926a47f70348bb228a5bc0472ca391f5
Yuke Wang <yuke_wang@ucsb.edu>
2022-01-31 16:57:23 -0800

    update with basic design

--- dgl_baseline/gcn.py
+++ dgl_baseline/gcn.py
@@ -23,12 +23,12 @@ class GCN(nn.Module):
         self.g = g
         self.layers = nn.ModuleList()
         # input layer
-        self.layers.append(GraphConv(in_feats, n_hidden, activation=activation, allow_zero_in_degree=True))
+        self.layers.append(GraphConv(in_feats, n_hidden, activation=activation, allow_zero_in_degree=True, norm='none', weight=False, bias=False))
         # hidden layers
         for i in range(n_layers - 2):
-            self.layers.append(GraphConv(n_hidden, n_hidden, activation=activation, allow_zero_in_degree=True))
+            self.layers.append(GraphConv(n_hidden, n_hidden, activation=activation, allow_zero_in_degree=True, norm='none', weight=False, bias=False))
         # output layer
-        self.layers.append(GraphConv(n_hidden, n_classes, allow_zero_in_degree=True))
+        self.layers.append(GraphConv(n_hidden, n_classes, allow_zero_in_degree=True, norm='none', weight=False, bias=False))
 
     def forward(self, features):
         h = features
