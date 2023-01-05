3a2939c984410332d101da4be9b0d44139d830a8
Yuke Wang <yuke_wang@ucsb.edu>
2023-01-04 17:00:09 -0800

    fix docker issue

--- TCGNN_conv/build.sh
+++ TCGNN_conv/build.sh
@@ -1,5 +1,5 @@
-# TORCH_CUDA_ARCH_LIST="8.6" python setup.py install
-rm -rf build/
-rm -rf dist/
-rm -rf *.egg-info
-python setup.py install
+TORCH_CUDA_ARCH_LIST="8.6" python setup.py install
+# rm -rf build/
+# rm -rf dist/
+# rm -rf *.egg-info
+# python setup.py install
